52fe559ea87421294b3cda44b66730387277c835
minsun <kor3334@naver.com>
2021-02-28 19:18:29 +0900

    CRUD 기능 완성

---
+++ index.js
@@ -0,0 +1,101 @@
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+
+// DB setting
+mongoose.set('useNewUrlParser', true);    
+mongoose.set('useFindAndModify', false);  
+mongoose.set('useCreateIndex', true);     
+mongoose.set('useUnifiedTopology', true); 
+mongoose.connect('mongodb+srv://minsun:dbsalstjs1@cluster0.uwree.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'); // 2
+var db = mongoose.connection;
+
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
+
+// Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
+
+//DB schema
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String},
+  phone:{type:String}
+});
+var Contact = mongoose.model('contact', contactSchema);
+
+//Route
+//Home
+app.get('/', function(req, res){
+  res.redirect('/contacts');
+})
+
+//Contact - Index 
+app.get('/contacts', function(req, res){
+  Contact.find({}, function(err, contacts){
+    if(err) return res.json(err);
+    res.render('contacts/index', {contacts:contacts});
+  });
+});
+
+//Contact - New
+app.get('/contacts/new', function(req,res){
+  res.render('contacts/new');
+});
+
+//Contacts - create
+app.post('/contacts', function(req,res){
+  Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
+
+//Contacts - show
+app.get('/contacts/:id', function(req,res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render('contacts/show', {contact:contact});
+  });
+});
+
+//Contacts - edit
+app.get('/contacts/:id/edit', function(req, res){
+  Contact.findOne({_id:req.params.id}, function(err, contact){
+    if(err) return res.json(err);
+    res.render('contacts/edit', {contact:contact});
+  });
+});
+
+//Contacts - update
+app.put('/contacts/:id', function(req,res){
+  Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts/' + req.params.id);
+  });
+});
+
+//Contacts - destroy
+app.delete('/contacts/:id', function(req,res){
+  Contact.deleteOne({_id:req.params.id}, function(err){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
+
+// Port setting
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
